73ad1205e44d85f845be9b9b1cf9bc1abbc6825f
fabiocingottini <fabio.cingottini@gmail.com>
2018-08-13 16:44:45 +0200

    refactor

--- setup.py
+++ setup.py
@@ -3,13 +3,18 @@
 from setuptools import setup
 
 setup(
-    name='cli_utils',
+    name='cli_interface_utils',
     version='0.1',
     description='create a CliInterface, extends AbstractCliWorker, now you have a working cli application',
-    url='http://github.com/',
+    url='https://github.com/FabioCingottini/cli_interface_utils',
     author='Fabio Cingottini',
     author_email='fabio.cingottini@gmail.com',
     license='MIT',
-    packages=['cli_utils'],
-    zip_safe=False
+    packages=['cli_interface_utils'],
+    zip_safe=False,
+    classifiers=(
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    )
 )
